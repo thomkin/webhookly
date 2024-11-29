1fdc58909b71764fe001620ab44fc0774c449209
Thomas <thkinder@gmail.com>
2024-11-29 08:11:34 +0530

    added basic code for push event handling

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 
 	"github.com/go-playground/webhooks/v6/github"
 	"gopkg.in/yaml.v2"
 )
 
+type Handler struct {
+	Ref  string
+	Path string
+}
+
+type Handlers map[string]Handler
+
 func main() {
 	var config struct {
-		Secret string `yaml:"secret"`
-		Cert   string `yaml:"cert"`
-		Key    string `yaml:"key"`
+		Secret   string   `yaml:"secret"`
+		Cert     string   `yaml:"cert"`
+		Key      string   `yaml:"key"`
+		Handlers Handlers `yaml:"handlers"`
 	}
 	data, err := os.ReadFile("config.yaml")
 	if err != nil {
@@ -27,7 +36,7 @@ func main() {
 
 	hook, _ := github.New(github.Options.Secret(config.Secret))
 	http.HandleFunc("/github", func(w http.ResponseWriter, r *http.Request) {
-		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
+		payload, err := hook.Parse(r, github.PushEvent, github.PullRequestEvent)
 		fmt.Printf("payload: %v\n", payload)
 		if err != nil {
 			if err == github.ErrEventNotFound {
@@ -39,8 +48,23 @@ func main() {
 
 		case github.PushPayload:
 			push := payload.(github.PushPayload)
-			// Do whatever you want from here...
-			fmt.Printf("Push:: %+v", push)
+			ref := push.Ref
+			handler, ok := config.Handlers[ref]
+			if !ok {
+				println("Handler not found for ref: " + ref)
+				return
+			}
+
+			if _, err := os.Stat(handler.Path); os.IsNotExist(err) {
+				log.Fatalf("Handler %s does not exist", handler.Path)
+			}
+
+			cmd := exec.Command(handler.Path)
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%s", output)
 
 		case github.PullRequestPayload:
 			pullRequest := payload.(github.PullRequestPayload)
