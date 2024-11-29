19ed58585ce74cce595e3edc2f1adead07dca6e8
Thomas <thkinder@gmail.com>
2024-11-29 09:36:40 +0530

    imporved install script

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ set -e
 
 BUILD_DIR="$(pwd)"
 EXECUTABLE="$BUILD_DIR/webhookly"
+CONFIG_FILE="/etc/webhookly/config.yaml"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "secret: 1234
+cert: /etc/ssl/star.pem
+key: /etc/ssl/star.key
+handlers:
+  refs/heads/main:
+    path: /tmp/execute.me.sh" > $CONFIG_FILE
+fi
 
 # build the current go code
 go build -o $EXECUTABLE .
@@ -16,7 +26,7 @@ After=network.target
 
 [Service]
 User=webhookly
-ExecStart=$EXECUTABLE
+ExecStart=$EXECUTABLE -config $CONFIG_FILE
 Restart=always
 RestartSec=10
 
@@ -26,3 +36,4 @@ WantedBy=multi-user.target" > $SERVICE_FILE
 # enable and start the service
 systemctl enable webhookly
 systemctl start webhookly
+
